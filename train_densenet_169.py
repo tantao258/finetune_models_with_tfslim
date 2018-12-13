3850e6a647aed7526e62b63671b906c8027da8b7
tantao258 <823232162@qq.com>
2018-12-13 04:24:03 -0800

    update

--- train_densenet_169.py
+++ train_densenet_169.py
@@ -18,7 +18,7 @@ tf.app.flags.DEFINE_string("train_file", './cifar_data/train.txt', "the path of 
 tf.app.flags.DEFINE_string("val_file", './cifar_data/validation.txt', "the path of val data")
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "learn_rate(default:0.001)")
 tf.app.flags.DEFINE_integer("num_epochs", 50, "num_epoches(default:10)")
-tf.app.flags.DEFINE_integer("batch_size", 3, "batch_size(default:128)")
+tf.app.flags.DEFINE_integer("batch_size", 16, "batch_size(default:128)")
 tf.app.flags.DEFINE_integer("num_classes", 10, "num_classes(default:2)")
 tf.app.flags.DEFINE_float("keep_prob", 0.8, "dropout_rate(default:0.8)")
 tf.app.flags.DEFINE_integer("evaluate_every", 200, "Evaluate model on dev set after this many steps (default: 100)")
